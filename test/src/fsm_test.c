b4a5743bf294f70e26cc0e476950b56291e2fd25
Tom <tom.christ@stud.h-da.de>
2025-09-27 19:18:46 +0200

    Added Test

--- test/src/fsm_test.c
+++ test/src/fsm_test.c
@@ -1,8 +1,6 @@
-#include "example.h"
+#include "fsm_test.h"
 
-#include "esp_log.h"
-
-#define TAG "EXAMPLE"
+#include <stdio.h>
 
 /******************************************************************************/
 /* FUNCTION PREDECLARATION FOR FSM                                            */
@@ -22,7 +20,8 @@ static fsm_t fsmSub = {0};
 /*** MAIN STATEMACHINE ***/
 static const fsm_cfg_t fsmMainCfg = {
     .initialState = FSM_STATE_MAIN_1,
-    .states = {
+    .statesCount = 3,
+    .states = (const fsm_state_cfg_t[3]){
 
         /*** MAIN STATE 1 *****************************************************/
         {
@@ -30,7 +29,8 @@ static const fsm_cfg_t fsmMainCfg = {
             .entryAction = {myLog, (fsm_arg_t) "MAIN: STATE1: ENTRY"},
             .doAction = {myLog, (fsm_arg_t) "MAIN: STATE1: DO"},
             .exitAction = {myLog, (fsm_arg_t) "MAIN: STATE1: EXIT"},
-            .transitions = (const fsm_transition_cfg_t[]){
+            .transitionsCount = 1,
+            .transitions = (const fsm_transition_cfg_t[1]){
 
                 /*** EVENT 2 ***/
                 {
@@ -47,7 +47,8 @@ static const fsm_cfg_t fsmMainCfg = {
             .entryAction = {myLog, (fsm_arg_t) "MAIN: STATE2: ENTRY"},
             .doAction = {myLog, (fsm_arg_t) "MAIN: STATE2: DO"},
             .exitAction = {myLog, (fsm_arg_t) "MAIN: STATE2: EXIT"},
-            .transitions = (const fsm_transition_cfg_t[]){
+            .transitionsCount = 3,
+            .transitions = (const fsm_transition_cfg_t[3]){
 
                 /*** EVENT 1 ***/
                 {
@@ -60,6 +61,7 @@ static const fsm_cfg_t fsmMainCfg = {
                 {
                     .event = FSM_EVENT_2,
                     .action = {myLog, (fsm_arg_t) "MAIN: STATE2: EVENT2"},
+                    .toState = FSM_STATE_MAIN_2,
                 },
 
                 /*** EVENT 3 ***/
@@ -75,7 +77,9 @@ static const fsm_cfg_t fsmMainCfg = {
         {
             .state = FSM_STATE_MAIN_SUB,
             .entryAction = {resetFsm, (fsm_arg_t)&fsmSub},
-            .transitions = (const fsm_transition_cfg_t[]){
+            .subFsm = &fsmSub,
+            .transitionsCount = 1,
+            .transitions = (const fsm_transition_cfg_t[1]){
                 /*** EVENT 3 ***/
                 {
                     .event = FSM_EVENT_3,
@@ -90,7 +94,8 @@ static const fsm_cfg_t fsmMainCfg = {
 /*** SUB STATEMACHINE ***/
 static const fsm_cfg_t fsmSubCfg = {
     .initialState = FSM_STATE_SUB_1,
-    .states = {
+    .statesCount = 2,
+    .states = (const fsm_state_cfg_t[2]){
 
         /*** SUB STATE 1 ******************************************************/
         {
@@ -98,12 +103,14 @@ static const fsm_cfg_t fsmSubCfg = {
             .entryAction = {myLog, (fsm_arg_t) "SUB: STATE1: ENTRY"},
             .doAction = {myLog, (fsm_arg_t) "SUB: STATE1: DO"},
             .exitAction = {myLog, (fsm_arg_t) "SUB: STATE1: EXIT"},
-            .transitions = (const fsm_transition_cfg_t[]){
+            .transitionsCount = 2,
+            .transitions = (const fsm_transition_cfg_t[2]){
 
                 /*** EVENT 1 ***/
                 {
                     .event = FSM_EVENT_1,
                     .action = {myLog, (fsm_arg_t) "SUB: STATE1: EVENT1"},
+                    .toState = FSM_STATE_SUB_1,
                 },
 
                 /*** EVENT 2 ***/
@@ -121,7 +128,8 @@ static const fsm_cfg_t fsmSubCfg = {
             .entryAction = {myLog, (fsm_arg_t) "SUB: STATE2: ENTRY"},
             .doAction = {myLog, (fsm_arg_t) "SUB: STATE2: DO"},
             .exitAction = {myLog, (fsm_arg_t) "SUB: STATE2: EXIT"},
-            .transitions = (const fsm_transition_cfg_t[]){
+            .transitionsCount = 1,
+            .transitions = (const fsm_transition_cfg_t[1]){
 
                 /*** EVENT 1 ***/
                 {
@@ -137,22 +145,62 @@ static const fsm_cfg_t fsmSubCfg = {
 /******************************************************************************/
 /* FUNCTION IMPLEMENTATIONS                                                   */
 /******************************************************************************/
-void run_example(void)
+int main(void)
 {
-    ESP_LOGI(TAG, "example started");
+    printf("main: started\n");
     fsm_RC_t res = FSM_RC_ERROR;
 
+    /* Initialize the statemachines */
     res = fsm_init(&fsmMain, &fsmMainCfg);
     if (res != FSM_RC_OK)
     {
-        ESP_LOGE(TAG, "example: error initializing main fsm");
-        return;
+        printf("ERROR: main: error initializing main fsm\n");
+        return -1;
     }
-
+    printf("main: main fsm initialized\n");
+    
     res = fsm_init(&fsmSub, &fsmSubCfg);
     if (res != FSM_RC_OK)
     {
-        ESP_LOGE(TAG, "example: error initializing sub fsm");
+        printf("ERROR: main: error initializing sub fsm\n");
+        return -1;
+    }
+    printf("main: sub fsm initialized\n");
+
+    printf("main: simulating events\n");
+    /* Simulate events to go through the statemachine */
+    fireEvent(FSM_EVENT_1); /* stay in main state 1*/
+    fireEvent(FSM_EVENT_3); /* stay in main state 1*/
+    fireEvent(FSM_EVENT_2); /* go to main state 2*/
+    fireEvent(FSM_EVENT_2); /* stay in main state 2*/
+    fireEvent(FSM_EVENT_1); /* go back to main state 1*/
+    fireEvent(FSM_EVENT_3); /* stay in main state 1, test not implemented event */
+    fireEvent(FSM_EVENT_2); /* go to main state 2*/
+    fireEvent(FSM_EVENT_3); /* go to sub state (sub fsm) */
+    fireEvent(FSM_EVENT_1); /* stay in sub state 1*/
+    fireEvent(FSM_EVENT_2); /* go to sub state 2*/
+    fireEvent(FSM_EVENT_2); /* stay in sub state 2*/
+    fireEvent(FSM_EVENT_1); /* go back to sub state 1*/
+    fireEvent(FSM_EVENT_3); /* leave the sub fsm, back to main state 2 */
+    fireEvent(FSM_EVENT_3); /* go back to sub state (sub fsm) */
+    fireEvent(FSM_EVENT_1); /* stay in sub state 1*/
+    printf("main: simulating done\n");
+    return 0;
+}
+
+void fireEvent(fsm_event_t i_event)
+{
+    fsm_RC_t res = FSM_RC_ERROR;
+
+    uint32_t eventNum = (uint32_t)i_event + 1;
+
+    printf("fireEvent: EVENT%d\n", eventNum);
+
+    /* Fire event in main statemachine */
+    res = fsm_process(&fsmMain, i_event);
+    if (res != FSM_RC_OK)
+    {
+        printf("ERROR: fireEvent: error firing event %d in main fsm\n", i_event);
         return;
     }
 }
@@ -160,7 +208,7 @@ void run_example(void)
 static void myLog(fsm_arg_t i_message)
 {
     const char *messageStr = (const char *)i_message;
-    ESP_LOGI(TAG, "%s", messageStr);
+    printf("myLog: %s\n", messageStr);
 }
 
 static void resetFsm(fsm_arg_t i_fsm)
@@ -168,9 +216,12 @@ static void resetFsm(fsm_arg_t i_fsm)
     fsm_t *const fsm = (fsm_t *const)i_fsm;
     if (fsm == NULL)
     {
-        ESP_LOGE(TAG, "resetFsm: nullptr");
+        printf("ERROR: resetFsm: nullptr\n");
         return;
-        fsm_reset(fsm);
-        ESP_LOGI(TAG, "SUB: FSM reset");
+    }
+    fsm_RC_t res = fsm_reset(fsm);
+    if (res != FSM_RC_OK)
+    {
+        printf("ERROR: resetFsm: nullptr\n");
     }
 }
---
+++ test/src/fsm_test.h
@@ -0,0 +1,10 @@
+#ifndef FSM_TEST_H_
+#define FSM_TEST_H_
+
+#include "fsm.h"
+
+int main(void);
+
+void fireEvent(fsm_event_t i_event);
+
+#endif
